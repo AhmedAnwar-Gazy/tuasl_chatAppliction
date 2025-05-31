9f6cde417cbc57c6e41c6fa8f2f54eb76a3e6107
MubarakAshrafAlrawy <mubarakashrafalrawy@gmail.com>
2025-05-31 14:59:48 +0300

    uploading changs

--- clinent_basic/clint_pages/src/main/java/orgs/clint_pages/MainApp.java
+++ clinent_basic/clint_pages/src/main/java/orgs/clint_pages/MainApp.java
@@ -3,14 +3,10 @@ package orgs.clint_pages;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-import orgs.clint_pages.utils.DataBaseConnection;
 import orgs.clint_pages.utils.Navigation;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 //import static jdk.internal.foreign.SystemLookup.WindowsFallbackSymbols.printf;
 
@@ -20,26 +16,6 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws IOException, SQLException {
-        DataBaseConnection db = new DataBaseConnection();
-        Connection conn =  db.getConnection();
-        String sql = "Create Table If not exists my_table(id Integer primary key,email TEXT)";
-        Statement s = conn.createStatement();
-        if(s.executeUpdate(sql) > 0){
-            System.out.println("Table Created");
-        }
-        sql = "Insert into my_table (email) values('ahmed anwar@email.com'); ";
-        if(s.executeUpdate(sql) > 0){
-            System.out.println("One Row Inserted");
-        }
-        sql = "select id,email from my_table;";
-        ResultSet rs = s.executeQuery(sql);
-        while (rs.next()){
-            int id = rs.getInt("id");
-            String email = rs.getString("email");
-//            printf("Column getted values: id: %d and email is: %s",id,email);
-            System.out.println("your id is : "+id+" and email is : "+email+" ");
-        }
-
         this.primaryStage = stage;
         this.primaryStage.setTitle("Chat Application");
 
@@ -53,8 +29,6 @@ public class MainApp extends Application {
         primaryStage.setMinWidth(800); // Minimum responsive width
         primaryStage.setMinHeight(600); // Minimum responsive height
         primaryStage.show();
-
-        db.closeConnection();
     }
 
     public static void main(String[] args) {
